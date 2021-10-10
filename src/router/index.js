d3aa96d38aa0b6c9c0411a368fda4aa4f7be42d2
= <joao.souza@avenuecode.com>
2021-10-10 09:30:54 -0300

    Class Routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import ClassProfile from '../views/ClassProfile.vue'
+import CreateClass from '../views/CreateClass.vue'
 
 Vue.use(VueRouter)
 
@@ -13,8 +15,18 @@ const routes = [{
   {
     path: '/Home',
     name: 'Home',
-    component: Home
-  }
+    component: Home,
+    children: [{
+        path: 'ClassProfile',
+        component: ClassProfile,
+      },
+      {
+        path: 'CreateClass',
+        component: CreateClass,
+      }
+    ]
+  },
+
 ]
 
 const router = new VueRouter({
