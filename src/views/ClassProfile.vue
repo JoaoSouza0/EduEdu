18a50aebe545126d4332ee7e8b54b605d4e1d9f8
= <joao.souza@avenuecode.com>
2021-10-10 09:31:07 -0300

    Class pages

---
+++ src/views/ClassProfile.vue
@@ -0,0 +1,78 @@
+<template>
+    <section>
+        <div class="input-filters">
+            <input type="text" name="" id="" placeholder="Pesquisar" />
+            <div class="filters-container">
+                <label for="">Filtrar por:</label>
+                <div>
+                    <button type="submit" class="btn-light filter">Nome</button>
+                    <button type="submit" class="btn-dark filter">Ano</button>
+                </div>
+            </div>
+            <button class="btn-dark btn-add-aluno">＋ Adicionar novo aluno</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ClassProfile'
+};
+</script>
+
+<style scoped>
+.input-filters {
+    padding: 31px 0;
+    background-color: #fff;
+    width: 1258px;
+    margin: 47px auto;
+    border: 1px solid #e1e1e1;
+    box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.1) ;
+    box-sizing: border-box;
+    border-radius: 25px;
+    display: flex;
+    justify-content: space-around;
+}
+
+.input-filters input {
+    width: 310px;
+    padding: 13px 10px;
+    border: 1px solid #c3c3c3;
+    border-radius: 10px;
+}
+.btn-add-aluno {
+    width: 330px;
+    font-size: 22px;
+    font-weight: bold;
+}
+
+.filters-container {
+    position: relative;
+    align-self: flex-end;
+}
+
+.filters-container label {
+    position: absolute;
+    font-weight: 600;
+    font-size: 14px;
+    background-color: transparent;
+    top: -20px;
+    left: 8px;
+}
+
+.filter {
+    border: 1px solid #e1e1e1;
+    width: 160px;
+    padding: 8px 0;
+    box-shadow: none;
+}
+.btn-light.filter {
+    position: relative;
+    left: 15px;
+}
+
+.btn-dark.filter {
+    position: relative;
+    right: 15px;
+}
+</style>
---
+++ src/views/CreateClass.vue
@@ -0,0 +1,74 @@
+<template>
+    <section>
+        <div class="form-container">
+            <form action="">
+                <div class="avatar-photo">
+                    <img src="../assets/icons/icon_user.png" class="tryio" />
+                    <label class="btn-light" for="avatar">
+                        tryi
+                    </label>
+                    <input type="file" id="avatar" name="avatar" @change="loadImg" accept="image/png, image/jpeg" />
+                </div>
+                <div>
+                    <label for=""></label>
+                    <input type="text" name="" id="" />
+                    <label for=""></label>
+                    <input type="text" name="" id="" />
+                </div>
+
+                <div>
+                    <div id="bm"></div>
+                    <input type="range" name="" id="" />
+                    <p></p>
+                    <p></p>
+                </div>
+
+                <div>
+                    <input type="checkbox" name="check" id="check" />
+                </div>
+
+                <button class="btn-dark">Adicionar aluno</button>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: 'CreateClass',
+    data() {
+        return {
+            animationOn: false
+        };
+    },
+    methods: {
+        loadImg(e) {
+            console.log(e.target.files);
+            const fr = new FileReader();
+            fr.onload = () => {
+                document.querySelector('.tryio').src = fr.result;
+            };
+            fr.readAsDataURL(e.target.files[0]);
+        },
+        testLottie() {}
+    },
+    created() {}
+};
+</script>
+
+<style>
+.form-container {
+    margin: 93px auto;
+    width: 670px;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.form-container div {
+    border: 1px solid red;
+    width: 590px;
+}
+</style>
