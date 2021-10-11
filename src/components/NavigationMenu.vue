902e2d0538133a5ed5d642852cadf3c9a3c481ec
= <joao.souza@avenuecode.com>
2021-10-11 10:13:01 -0300

    Creating component navigation menu

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="navigation-container">
+        <div class="img-container">
+            <img src="../../src/assets/logo.png" alt="Logo EduEdu" />
+        </div>
+        <nav class="navigation">
+            <ul>
+                <li class="nav-item">
+                    <router-link to="/Home/ClassProfile">
+                        <img src="../../src/assets/icons/icon_face.png" alt="sas" />
+                        Perfil dos Alunos
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/Home/PortugueseProve">
+                        <img src="../../src/assets/icons/icon_doc.png" alt="sas" />
+                        Prova de Português
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/Home/DigitalActivities">
+                        <img src="../../src/assets/icons/icon_test.png" alt="sas" />
+                        Atividades Digitais
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/Home/Help">
+                        <img src="../../src/assets/icons/icon_help.png" alt="sas" />
+                        Ajuda
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/Home/Settings">
+                        <img src="../../src/assets/icons/icon_settings.png" alt="sas" />
+                        Configurações
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavigationMenu'
+};
+</script>
+
+<style scoped>
+
+
+.navigation-container {
+    min-width: 550px;
+    background-color: #ffff;
+    margin-right: 10px;
+    box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
+    height: 100vh;
+}
+
+.img-container img {
+    padding: 68px 0;
+    margin: 0 auto;
+}
+
+.navigation a {
+    width: 91%;
+    display: flex;
+    align-items: center;
+    padding: 29px 0 29px 51px;
+    margin-bottom: 30px;
+}
+.navigation li img {
+    padding: 0 30px;
+}
+
+.navigation a {
+    font-weight: bold;
+    font-size: 20px;
+    color: #cacaca;
+}
+nav a.router-link-exact-active {
+    background-color: var(--light-blue);
+    color: #fff;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,6 @@
 <template>
     <section class="page-container">
-        <div class="navigation-container">
-            <div class="img-container">
-                <img src="../../src/assets/logo.png" alt="Logo EduEdu" />
-            </div>
-            <nav class="navigation">
-                <ul>
-                    <li class="nav-item" v-for="({ name }, index) in navItens" :key="index">
-                        <router-link to="/Home/ClassProfile">
-                            <img src="../../src/assets/icons/icon_doc.png" alt="sas" />
-                            {{ name }}
-                        </router-link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <NavigationMenu />
         <div class="router-view-container">
             <router-view></router-view>
         </div>
@@ -22,60 +8,21 @@
 </template>
 
 <script>
+import NavigationMenu from '@/components/NavigationMenu.vue';
 export default {
     name: 'Home',
-    data() {
-        return {
-            navItens: [
-                { name: 'Perfil dos Alunos', iconSrc: '../../src/assets/icons/icon_face.png' },
-                { name: 'Prova de Português', iconSrc: '../../src/assets/icons/icon_doc.png' },
-                { name: 'Atividades Digitais', iconSrc: '../../src/assets/icons/icon_test.png' },
-                { name: 'Ajuda', iconSrc: '../../src/assets/icons/icon_help.png' },
-                { name: 'Configurações', iconSrc: '../../src/assets/icons/icon_.settings.png' }
-            ]
-        };
+    components: {
+        NavigationMenu
     }
 };
 </script>
 
-<style scoped>
+<style>
 .page-container {
     display: flex;
 }
-
-.navigation-container {
-    width: 550px;
-    background-color: #ffff;
-    box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-}
-
-.img-container img {
-    padding: 68px 0;
-    margin: 0 auto;
-}
-
-.navigation a {
-    width: 91%;
-    display: flex;
-    align-items: center;
-    padding: 29px 0 29px 51px;
-    margin-bottom: 30px;
-}
-.navigation li img {
-    padding: 0 30px;
-}
-
-.navigation a {
-    font-weight: bold;
-    font-size: 20px;
-    color: #cacaca;
-}
-nav a.router-link-exact-active {
-    background-color: var(--light-blue);
-    color: #fff;
+.page-container div {
+    display: inline;
 }
 .router-view-container {
     width: 100%;
