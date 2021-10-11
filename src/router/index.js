8d8a90585ffe0fa04d473573eab381de1daa2725
= <joao.souza@avenuecode.com>
2021-10-11 10:13:15 -0300

    new possible routes of aplication

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,55 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import ClassProfile from '../views/ClassProfile.vue'
-import CreateClass from '../views/CreateClass.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import ClassProfile from '../views/ClassProfile.vue';
+import CreateClass from '../views/CreateClass.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [{
-    path: '/',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/Home',
-    name: 'Home',
-    component: Home,
-    children: [{
-        path: 'ClassProfile',
-        component: ClassProfile,
-      },
-      {
-        path: 'CreateClass',
-        component: CreateClass,
-      }
-    ]
-  },
-
-]
+const routes = [
+    {
+        path: '/',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/Home',
+        name: 'Home',
+        component: Home,
+        children: [
+            {
+                path: 'ClassProfile',
+                component: ClassProfile
+            },
+            {
+                path: 'CreateStudent',
+                component: CreateClass
+            },
+            {
+                path: 'PortugueseProve',
+                component: CreateClass
+            },
+            {
+                path: 'DigitalActivities',
+                component: CreateClass
+            },
+            {
+                path: 'Help',
+                component: CreateClass
+            },
+            {
+                path: 'Settings',
+                component: CreateClass
+            }
+        ]
+    }
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+});
 
-export default router
+export default router;
