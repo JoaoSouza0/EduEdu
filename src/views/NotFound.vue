9362d3e81ad536a0e2b5a0ace257fa2f0df9b17e
= <joao.souza@avenuecode.com>
2021-10-12 17:04:18 -0300

    NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,13 +1,40 @@
 <template>
-  <h1>Not Found</h1>
+    <section class="notfound-container ">
+        <div class="not-found-component">
+            <h1>Sorry we can't found this page</h1>
+            <img src="../assets/sorry-gif.gif" alt="EduEdu Logo" />
+        </div>
+    </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.notfound-container {
+    height: 100vh;
+    background: url('../../src/assets/login_bg.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.not-found-component {
+    width: 370px;
+    padding: 50px;
+    background: #ffffff;
+    border: 1px solid #e1e1e1;
+    box-shadow: 0px 4px 0px #e3e3e3;
+    border-radius: 25px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
 
+h1{
+  text-align: center;
+  color: #9b9a9a;
+}
 </style>
