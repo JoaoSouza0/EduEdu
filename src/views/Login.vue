c77a6d6f6b55f6065a46989f91f3a612765bd3fc
= <joao.souza@avenuecode.com>
2021-10-12 13:45:58 -0300

    prepering to fireAuth

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,32 @@
 <template>
-        <login-component />
+    <LoginComponent>
+        <button class="btn-light" type="submit" @click="out">Esqueci Senha</button>
+        <button class="btn-light" type="submit" @click="register">Create User</button>
+    </LoginComponent>
 </template>
 
 <script>
 import LoginComponent from '@/components/LoginComponent.vue';
+
 export default {
     components: { LoginComponent },
     name: 'LoginPage',
     component: {
         LoginComponent
-    }
+    },
+    computed: {
+        email() {
+            return this.$store.state.Login.auth.email;
+        },
+        password() {
+            return this.$store.state.Login.auth.password;
+        }
+    },
+    methods: {
+    
+    },
+    created() {}
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
